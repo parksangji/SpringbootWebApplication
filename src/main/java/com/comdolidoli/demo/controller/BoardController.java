d79eaf00641f933dd36693bce0516920e563ae01
sangji <parksangji1109@gmail.com>
2022-06-30 15:45:55 +0900

    [update] posting search

--- src/main/java/com/comdolidoli/demo/controller/BoardController.java
+++ src/main/java/com/comdolidoli/demo/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.comdolidoli.demo.model.Board;
 import com.comdolidoli.demo.repository.BoardRepository;
 import com.comdolidoli.demo.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,33 +27,37 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
-        model.addAttribute("boards",boards);
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = 1;
+        int endPage = boards.getTotalPages();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
     @GetMapping("/form")
-    public String form(Model model, @RequestParam(required = false) Long id){
-        model.addAttribute("board",new Board());
-        if(id == null){
-            model.addAttribute("board",new Board());
-        }
-        else{
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
             Board board = boardRepository.findById(id).orElse(null);
-            model.addAttribute("board",board);
+            model.addAttribute("board", board);
         }
         return "board/form";
     }
-    @PostMapping("/form")
-    public String createform(@Valid Board board, BindingResult bindingResult){
 
-        boardValidator.validate(board,bindingResult);
-        if(bindingResult.hasErrors())
-        {
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
             return "board/form";
         }
         boardRepository.save(board);
-        return "redirect:list";
+        return "redirect:/board/list";
     }
+
 }
--- src/main/java/com/comdolidoli/demo/model/Board.java
+++ src/main/java/com/comdolidoli/demo/model/Board.java
@@ -19,6 +19,8 @@ public class Board {
     @NotNull
     @Size(min = 2,max = 30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
+
+
     private String author;
 
     private String content;
